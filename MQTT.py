f7fcb641131845768527c443c7799f8c471579cb
raaghul-ravi <69359977+raaghul-ravi@users.noreply.github.com>
2020-08-27 18:30:48 +0530

    Update MQTT.py

--- MQTT.py
+++ MQTT.py
@@ -15,31 +15,36 @@ with open(private_key_file, "r") as f:
      private_key = f.read()
 
 with open(certificate_file, "r") as f:
-     certificate = f.read()   
+     certificate = f.read()
+     
 
-# Setup WiFi connection.
-wlan = network.WLAN( network.STA_IF )
-wlan.active( True )
-wlan.connect("SSID", "Password" )
-time.sleep(3)
-print('wifi Connected')
-while not wlan.isconnected():
-  machine.idle()
+class mqtt:
+    
+    def __init__(self):              
+        # Setup WiFi connection.
+        #self.wlan = network.WLAN( network.STA_IF )
+        #self.wlan.active( True )
+        #self.wlan.connect("username", "password" )
+        #time.sleep(3)
+        #print('wifi Connected')
+        #while not self.wlan.isconnected():
+          #machine.idle()
+        # Connect to MQTT broker.
+        self.mqtt = MQTTClient( client_id="esp32_Upython",
+                           server=HOST,
+                           port = 8883,
+                           keepalive = 10000,
+                           ssl = True,
+                           ssl_params={"cert": certificate,
+                                       "key": private_key,
+                                       "server_side":False }
+                           )
+        time.sleep(4)
+        self.mqtt.connect()
+        print('Connected to AWS cloud')
+        
+    def mqtt_publish(self, message):   
 
-# Connect to MQTT broker.
-mqtt = MQTTClient( client_id="esp32_Upython",
-                   server=HOST,
-                   port = 8883,
-                   keepalive = 10000,
-                   ssl = True,
-                   ssl_params={"cert": certificate,
-                               "key": private_key,
-                               "server_side":False }
-                   )
-time.sleep(8)
-mqtt.connect()
-
-# Publish a test MQTT message.
-mqtt.publish( topic = 'test', msg = 'Smart IoT', qos = 0 )
-
+        # Publish a MQTT message.
+        self.mqtt.publish( topic = 'Attendance', msg = message , qos = 0 )
 
